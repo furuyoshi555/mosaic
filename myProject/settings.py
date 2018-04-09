c5f1e385b5f1a8ad4a7b8d52f472e504965a6b28
FURUHASHI YOSHINORI <yutthi555@gmail.com>
2018-04-09 19:21:58 +0900

    debugtoolbarを削除

--- myProject/settings.py
+++ myProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "debug_toolbar",
+    # "debug_toolbar",
     "social_django",
     "mosaic_app",
 ]
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'myProject.urls'
@@ -158,7 +158,7 @@ LOGIN_REDIRECT_URL = 'my_page'
 LOGOUT_REDIRECT_URL = "index"
 
 
-INTERNAL_IPS = ["127.0.0.1"]
+# INTERNAL_IPS = ["127.0.0.1"]
 
 
 AUTHENTICATION_BACKENDS = (
