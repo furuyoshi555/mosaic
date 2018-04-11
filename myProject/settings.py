96263192e7ee123a2c3ceb2a0c341d6f0f161bc1
FURUHASHI YOSHINORI <yutthi555@gmail.com>
2018-04-11 21:13:02 +0900

    settings.pyの変更

--- myProject/settings.py
+++ myProject/settings.py
@@ -175,11 +175,11 @@ AUTHENTICATION_BACKENDS = (
 
 # Parse database configuration from $DATABASE_URL
 # import dj_database_url
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
-
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
