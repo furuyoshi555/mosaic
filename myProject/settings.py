5b92093c701c876d7632c84b9d999bfdc87fbb3f
FURUHASHI YOSHINORI <yutthi555@gmail.com>
2018-04-12 09:03:43 +0900

    DEBUGの変更

--- myProject/settings.py
+++ myProject/settings.py
@@ -26,7 +26,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,"media")
 SECRET_KEY = 'l#o20hu=#1!k0xk_4sap)*^ye_fiqsdgpo-@foczke79e%iza3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
